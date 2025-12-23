3c9be8051f1be5737b137be9155033c19cb0fd4b
RAHI BOT BBY <rahiahmed143@gmail.com>
2025-12-23 18:51:37 +0600

    Update gdbye.js

--- grpUpCmd/gdbye.js
+++ grpUpCmd/gdbye.js
@@ -4,7 +4,7 @@ module.exports = {
     const removedMembers = event.participants;
     for (const member of removedMembers) {
       await api.sendMessage(event.id, {
-        text: `Goodbye @${member.split('@')[0]}, we'll miss you!`,
+        text: `Goodbye @${member.split('@')[0]}, ⚡ Server Alert:⁨ disconnected, bandwidth free হলো ⚡`,
         mentions: [member]
       });
     }
